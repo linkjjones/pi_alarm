2f1b7115755cf69a99059b819fc0da7837b710b8
linkjjones <linkjjones@gmail.com>
2019-01-23 22:07:20 -0500

    Changed GPIO pin to 25 for realy. Edited messages

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # -- Alarm Settings
-PIN = 10                       # Pin to which the PowerSwitch Tail is connected
+PIN = 25                       # Pin to which the PowerSwitch Tail is connected
 ALARM_DURATION = 15            # Light duration in minutes
 
 # -- General Config
@@ -18,9 +18,8 @@ MESSAGES = [
     "You're smart AND pretty!",
     "You have a winning smile!",
     "You're home now; kick back and relax!",
-    "Don't forget: Andrew loves you!",
+    "Your children arise and call you blessed.",
     "What's for breakfast?",
-    "Show those kids who's boss!",
     "Grab a stuffed animal or husband, because it's time to cuddle!",
     "Honey, did you just fart?",
     "Did you lock the front door?",
@@ -28,14 +27,10 @@ MESSAGES = [
     "Thanks for putting up with me <3",
     "Violets are blue, roses are red. Turn off that light because it's time for bed!",
     "If you're happy and you know it go to sleep!",
-    "Poo Poo on the Motley Crew",
     "A hug! A hug! My kingdom for a hug!",
     "Did you get your bedtime kiss yet?",
     "You have a lovely voice! But be quiet now because it's time for bed.",
-    "A penny saved is a penny earned. Too bad Andrew's spending all your pennies on his startup!",
-    "Do you think Tinkerbell can cast Wingardium Leviosa?",
-    "Country fried steak or Chicken fried steak. What's the difference?",
-    "I want a Tesla. Too bad I don't drive much anymore. Or have any money.",
-    "Andrew loves you!",
-    "Ziggy loves you, too!"
+    "My amazing Ozi...",
+    "Jeffrey loves you!",
+    "Jesus loves you!"
 ]
